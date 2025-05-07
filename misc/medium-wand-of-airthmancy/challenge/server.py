5770a66867d9de50d1297425ddf77ca0804492cd
Mehloul Mohamed <m.mehloul@esi-sba.dz>
2025-05-07 01:55:33 +0100

    fixes

---
+++ misc/medium-wand-of-airthmancy/challenge/server.py
@@ -0,0 +1,33 @@
+scroll = input("Whisper the Incantation: ")
+allowed_runes = "0123456789engorgio "
+
+# No Dark Magic (No RCE filter)
+parchment = ""
+for rune in scroll:
+    if rune in allowed_runes:
+        parchment += rune
+    elif ord(rune) <= 127 and rune.isupper():
+        parchment += rune
+
+# Trim the spell to fit the Standard Book of Spells
+parchment = parchment[0:22]
+print(parchment)
+if parchment == "":
+    print("A silent wand does no magic...")
+    exit()
+
+magic = 1
+try:
+    magic = eval(parchment)
+    if magic <= 0:
+        print("Only positive energy fuels spells.")
+        exit()
+except:
+    print('wierd magic value...')
+    pass
+print('sfv:',magic)
+if magic and magic > 0:
+    print("Your charm fizzled... Try a stronger spell.")
+    exit()
+else:
+    print(eval(scroll))
